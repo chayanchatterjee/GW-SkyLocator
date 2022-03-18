fe6dc5cd5bfa3725668f17cc6a0651374740bc2e
Chayan Chatterjee <chayanchatterjee1994@gmail.com>
2022-03-18 13:40:29 +0800

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -31,11 +31,15 @@ CFG = {
                 "path_train_low_snr_1": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_snr_series_GW170817_BNS_snr-10to20_train_2k_samples.hdf",
                 "path_train_low_snr_2": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_snr_series_GW170817_BNS_snr-10to20_train_4k_samples.hdf",
                 "path_train_low_snr_3": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_snr_series_GW170817_BNS_snr-10to20_train_OzSTAR.hdf",
+            
+                "path_train_2_det_low_SNR": "/fred/oz016/Chayan/SNR_time_series_sample_files/Kaya_data/default_snr_series_GW170817_BNS_2_det_snr-10to20_low_mass.hdf",
                 
             
                 "path_test": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_snr_series_GW170817_BNS_test.hdf",
                 "path_test_low_SNR": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_snr_series_GW170817_BNS_snr_10-20_test.hdf",
-                "path_test_GW170817": "/group/pmc005/cchatterjee/Real_events/default_snr_series_GW170817_test_Gaussian_noise_1.hdf"
+                "path_test_GW170817": "/group/pmc005/cchatterjee/Real_events/default_snr_series_GW170817_test_Gaussian_noise_1.hdf",
+            
+                "path_test_2_det_low_SNR": "/fred/oz016/Chayan/SNR_time_series_sample_files/Kaya_data/default_snr_series_GW170817_2_det_snr_10-20_BNS_test.hdf",
                 },
     },
     "parameters": {
@@ -68,16 +72,21 @@ CFG = {
                 "path_train_low_snr_1": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_GW170817_parameters_BNS_train_snr-10to20_2k_samples.hdf",
                 "path_train_low_snr_2": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_GW170817_parameters_BNS_train_snr-10to20_4k_samples.hdf",
                 "path_train_low_snr_3": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_GW170817_snr-10to20_train_OzSTAR_parameters.hdf",
+            
+                "path_train_2_det_low_SNR": "/fred/oz016/Chayan/SNR_time_series_sample_files/Kaya_data/default_GW170817_BNS_2_det_snr-10to20_low_mass_parameters.hdf",
                 
             
                 "path_test": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_GW170817_parameters_BNS_test.hdf",
                 "path_test_low_SNR": "/group/pmc005/cchatterjee/SNR_time_series_sample_files/default_GW170817_parameters_test_snr_10-20_BNS_test.hdf",
-                "path_test_GW170817": "/group/pmc005/cchatterjee/Real_events/default_GW170817_parameters_test_Gaussian_noise_1.hdf"
+                "path_test_GW170817": "/group/pmc005/cchatterjee/Real_events/default_GW170817_parameters_test_Gaussian_noise_1.hdf",
+                
+                "path_test_2_det_low_SNR": "/fred/oz016/Chayan/SNR_time_series_sample_files/Kaya_data/default_GW170817_2_det_snr_10-20_BNS_test_parameters.hdf",
+                
                 },   
     },
     "train": {
-            "network": "ResNet-34",
-            "dataset": "BBH",
+            "network": "ResNet-34_2_det",
+            "dataset": "BNS",
             "test_real": False,
             "snr_range_train": 'low',
             "snr_range_test": 'low',
@@ -86,9 +95,9 @@ CFG = {
             "min_snr": 4,
             "n_samples": 410,
             "batch_size": 2000,
-            "output_filename": 'Adaptive_NSIDE/Injection_run_BBH_3_det_adaptive_SNR-10to20.hdf',
-            "num_detectors": 3,
-                "epochs": 50,
+            "output_filename": 'Adaptive_NSIDE/Injection_run_BNS_2_det_snr-10to20_low_mass.hdf',
+            "num_detectors": 2,
+                "epochs": 100,
             "validation_split": 0.05,
             "optimizer": {
                 "type": "adam"
@@ -119,6 +128,27 @@ CFG = {
                         "strides": 2,
                         "pool_size": 3,
                         "prev_filters": 32
+            
+            },
+            "ResNet_34_2_det": {
+                        "filters": 32,
+                        "kernel_size" : 7,
+                        "strides": 2,
+                        "pool_size": 2,
+                        "prev_filters": 32
+            
+            },
+            "LSTM_model": {
+                        "n_units": 128,
+                        "rate": 0.20
+            
+            },
+            "CNN_model": {
+                        "filters": 32,
+                        "kernel_size": 5,
+                        "max_pool_size": 2,
+                        "dropout_rate": 0.2,
+                        "n_units": 128
             
             },
             "learning_rate": 1e-4
