edf8c47ac2c436f1bf23ef445ba5ad495e49c700
Chayan Chatterjee <chayanchatterjee1994@gmail.com>
2022-08-01 23:29:54 +0800

    Update resnet_34_2_det.py

--- model/resnet_34_2_det.py
+++ model/resnet_34_2_det.py
@@ -31,22 +31,22 @@ class ResNet_34_2_det(ResidualUnit):
                                    padding='same', use_bias=False)(self.input1)
         X_real = tf.keras.layers.BatchNormalization()(X_real)
         X_real = tf.keras.layers.Activation('relu')(X_real)
-        X_real = tf.keras.layers.MaxPool1D(pool_size=self.pool_size, strides=self.strides, padding='same')(X_real)
+#        X_real = tf.keras.layers.MaxPool1D(pool_size=self.pool_size, strides=self.strides, padding='same')(X_real)
+        X_real = tf.keras.layers.Conv1D(self.filters_real, self.kernel_size, strides=self.strides,
+                                   padding='same', use_bias=False)(X_real)
         
-        for filters in [self.filters_real] * 3 + [self.filters_real*2] * 4 + [self.filters_real*4] * 6 + [self.filters_real*8] * 3:
+        for filters in [self.filters_real] * 3 + [self.filters_real*2] * 4 + [self.filters_real*4] * 4 + [self.filters_real*8] * 3:
+#        for filters in [self.filters_real] * 2 + [self.filters_real*2] * 2 + [self.filters_real*4] * 2 + [self.filters_real*8] * 2:
             strides = 1 if filters == self.prev_filters_real else 2
             X_real = ResidualUnit(filters, strides=strides)(X_real)
             self.prev_filters_real = filters
             
-#        X_real = tf.keras.layers.GlobalAvgPool1D()(X_real)
-##        flat1 = tf.keras.layers.Flatten()(X)
-        
-        X_real = tf.keras.layers.Conv1D(self.filters_real, self.kernel_size, strides=self.strides,
-                                   padding='same', use_bias=False)(X_real)
-        X_real = tf.keras.layers.BatchNormalization()(X_real)
-        X_real = tf.keras.layers.Activation('relu')(X_real)
-        X_real = tf.keras.layers.MaxPool1D(pool_size=self.pool_size, strides=self.strides, padding='same')(X_real)
-        
+        X_real = tf.keras.layers.GlobalAvgPool1D()(X_real)
+        flat1 = tf.keras.layers.Flatten()(X_real)
+#        dense1_real = tf.keras.layers.Dense(units=128, activation='relu')(flat1)
+#        dense2_real = tf.keras.layers.Dense(units=64, activation='relu')(flat1)
+
+#        drop1 = tf.keras.layers.Dropout(rate=0.15)(flat1)
         
         # Imaginary part
         
@@ -54,34 +54,36 @@ class ResNet_34_2_det(ResidualUnit):
                                    padding='same', use_bias=False)(self.input2)
         X_imag = tf.keras.layers.BatchNormalization()(X_imag)
         X_imag = tf.keras.layers.Activation('relu')(X_imag)
-        X_imag = tf.keras.layers.MaxPool1D(pool_size=self.pool_size, strides=self.strides, padding='same')(X_imag)
+#        X_imag = tf.keras.layers.MaxPool1D(pool_size=self.pool_size, strides=self.strides, padding='same')(X_imag)
+        X_imag = tf.keras.layers.Conv1D(self.filters_imag, self.kernel_size, strides=self.strides,
+                                   padding='same', use_bias=False)(X_imag)
         
-        for filters in [self.filters_imag] * 3 + [self.filters_imag*2] * 4 + [self.filters_imag*4] * 6 + [self.filters_imag*8] * 3:
+        for filters in [self.filters_imag] * 3 + [self.filters_imag*2] * 4 + [self.filters_imag*4] * 4 + [self.filters_imag*8] * 3:
+#        for filters in [self.filters_imag] * 2 + [self.filters_imag*2] * 2 + [self.filters_imag*4] * 2 + [self.filters_imag*8] * #2:
             strides = 1 if filters == self.prev_filters_imag else 2
             X_imag = ResidualUnit(filters, strides=strides)(X_imag)
             self.prev_filters_imag = filters
             
-#        X_imag = tf.keras.layers.GlobalAvgPool1D()(X_imag)
-##        flat2 = tf.keras.layers.Flatten()(X)
-         
-        X_imag = tf.keras.layers.Conv1D(self.filters_imag, self.kernel_size, strides=self.strides,
-                                   padding='same', use_bias=False)(X_imag)
-        X_imag = tf.keras.layers.BatchNormalization()(X_imag)
-        X_imag = tf.keras.layers.Activation('relu')(X_imag)
-        X_imag = tf.keras.layers.MaxPool1D(pool_size=self.pool_size, strides=self.strides, padding='same')(X_imag)
-        
+        X_imag = tf.keras.layers.GlobalAvgPool1D()(X_imag)
+        flat2 = tf.keras.layers.Flatten()(X_imag)
+#        dense1_imag = tf.keras.layers.Dense(units=128, activation='relu')(flat2)
+#        dense2_imag = tf.keras.layers.Dense(units=64, activation='relu')(flat2)
+
+#        drop2 = tf.keras.layers.Dropout(rate=0.15)(flat2)
+    
         # Merge features
         
-        merge = tf.keras.layers.concatenate([X_real, X_imag], axis=-1)
+#        merge = tf.keras.layers.concatenate([dense1_real, dense1_imag], axis=-1)
 
-#        merge = tf.keras.layers.concatenate([flat1, flat2])
+        merge = tf.keras.layers.concatenate([flat1, flat2])
         
         # Flatten features
         
-        flat = tf.keras.layers.Flatten()(merge)
+#        flat = tf.keras.layers.Flatten()(merge)
         
-        dense = tf.keras.layers.Dense(units=128, activation='relu')(flat)
+        dense1 = tf.keras.layers.Dense(units=256, activation='relu')(merge)
+#        dense2 = tf.keras.layers.Dense(units=128, activation='relu')(dense1)
         
-        return dense
+#        return flat
         
-    
+        return dense1
